ffde7c89699ff41b0e1de5740fdb797b7932d0ef
Miles <milescambron@gmail.com>
2017-07-18 14:13:40 -0400

    Added messages.

--- CodeCamp-Message.js
+++ CodeCamp-Message.js
@@ -8,13 +8,21 @@ module.exports = {
      * @param {Object} Slack
      */
     message_received: function(message, channelName, userName, Slack) {
-        // *********************************************************************
-        // CODE HERE!
-        // *********************************************************************
-
-        Slack.postMessageToChannel(channelName, 'Message received.');
-
-        // *********************************************************************
+    var mlc = message.toLowerCase();
+    var resp = "shhhhhhh";
+    if (mlc.includes("laser")) {
+        resp = "Not anymore, theres a blanket";
+    } else if (mlc.includes("like")) {
+        resp = "Good for you";
+    } else if (mlc.includes("i am dat boi")) {
+        resp = "yeah toast";
+    } else if (mlc.includes("2 to the 9")) {
+        resp = "i'll have a number 6 with Xtra dip";
+    } else if (mlc.includes("oh yeah")) {
+        resp = "mr krabbs AHHHHHHHHHHHHHHHHH";
+    }
+    Slack.postMessageToChannel(channelName, resp);
+       // *********************************************************************
         // STOP CODING!
         // *********************************************************************
     },
