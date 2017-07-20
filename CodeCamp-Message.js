3d5989b43606be4c2493fd8b0e9630fe662c7ea1
Grey <strongkydz@gmail.com>
2017-07-20 14:28:05 -0400

    fun

--- CodeCamp-Message.js
+++ CodeCamp-Message.js
@@ -63,13 +63,13 @@ module.exports = {
             } else if (mlc == "mustache") {
                 resp = "You must be David";
                 memory.lastquestion = "";
-                return;
+                // return;
             } else {
                 resp = "That's not a hair shade!  Please try again: Do you have *dark* or *light* hair?";
                 Slack.postMessageToChannel(channelName, resp);
                 return;
             }
-            // break;
+            break;
     }
 
    /**
